5075020e11492d7613c24953e908ec4ed1833a5a
Myles <landais.myles@gmail.com>
2020-07-24 22:19:18 -0500

    update banking logic

--- src/main/java/com/nebula/SkeletonScript.java
+++ src/main/java/com/nebula/SkeletonScript.java
@@ -7,7 +7,7 @@ import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.script.Category;
 
-@ScriptManifest(author = "Alexei", name = "DreamBot Skeleton Template Script", version = 1.0, description = "Example script for DreamBot", category = Category.MISC)
+@ScriptManifest(author = "Alexei", name = "DreamBot Skeleton Template Script", version = 1.1, description = "Example script for DreamBot", category = Category.MISC)
 public class SkeletonScript extends AbstractScript {
 
     private enum State{
@@ -15,8 +15,12 @@ public class SkeletonScript extends AbstractScript {
     }
 
     private State getAction(){
-        if( true ){
+        if( !getInventory().contains(303) || getInventory().isFull() ){
+            log("Need something from bank");
             return State.Bank;
+        }
+        if( getInventory().contains(303) ){
+            return State.Fish;
         }
         return State.Wait;
     }
@@ -33,18 +37,29 @@ public class SkeletonScript extends AbstractScript {
     public int onLoop() {
         switch (getAction()) {
             case Bank:
-                log("Walking to lumbridge bank");
+                log("Walking to Lumbridge bank");
                 if (!BankLocation.LUMBRIDGE.getArea(2).contains(getLocalPlayer())) {
                     if (getWalking().walk(BankLocation.LUMBRIDGE.getArea(2).getRandomTile())) {
                         MethodProvider.sleepUntil(() -> getWalking().shouldWalk(), 4000);
                     }
                 }
+                if(BankLocation.LUMBRIDGE.getArea(3).contains( getLocalPlayer()) ){
+                    if( !getBank().isOpen() ){
+                        MethodProvider.sleepUntil(() -> getBank().open( getBank().getClosestBankLocation()), 1000);
+                    }
+                    if( getInventory().isFull() ){
+                        getDepositBox().depositAllItems();
+                    }
+                    if( !getInventory().contains(303) ) {
+                        getBank().withdraw(303,1);
+                    }
+                    if(getBank().open()){
+                        getBank().close();
+                    }
+                }
+            case Fish:
+                log("TODO Fishing code");
         }
-        return Calculations.random(500, 600);
-    }
-
-    public int bank() {
-        return 0;
-        }
+        return Calculations.random(1000, 5000);
     }
 }
